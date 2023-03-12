f018c7a89e617135acc581336e05e7ccff24a697
mariia <111996419+MariiaS1710@users.noreply.github.com>
2023-03-12 03:57:27 +0100

    upd feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+import { throttle } from 'lodash';
+
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('.feedback-form input'),
+    message: document.querySelector('.feedback-form textarea'),
+};
+
+refs.form.addEventListener('input', throttle(e => {
+    const object = { email: refs.email.value, message: refs.message.value };
+    localStorage.setItem('feedback-form-state', JSON.stringify(object));
+}, 500)
+);
+
+refs.form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+    e.preventDefault();
+    
+    if (refs.email.value === "" || refs.message.value === "") {
+        return alert('Заповніть всі поля!'); 
+    }
+    e.currentTarget.reset();
+    localStorage.removeItem('feedback-form-state')
+}
+//    дописать функцию котора загружает страничку и записіввеет из стореджа туда данніе 
 
